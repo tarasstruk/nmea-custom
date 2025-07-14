9df40685d6ce95643f9e612bbd49a0f0e61ef0c2
Taras Struk <t.struk@sonnen.de>
2025-07-14 18:54:56 +0200

    improve semantics of private fn do_parse_nmea_sentence

--- src/parse.rs
+++ src/parse.rs
@@ -83,7 +83,7 @@ fn take_while_no_star(s: &str) -> IResult<&str, &str> {
     take_while(|c| c != '*')(s)
 }
 
-fn do_parse_nmea_sentence(i: &str) -> IResult<&str, NmeaSentence> {
+fn do_parse_nmea_sentence(i: &str) -> core::result::Result<NmeaSentence, Error<'_>> {
     let (i, talker_id) = preceded(char('$'), take(2usize))(i)?;
     let (i, message_id) = parse_sentence_type(i)?;
     let (i, _) = char(',')(i)?;
@@ -102,14 +102,14 @@ fn do_parse_nmea_sentence(i: &str) -> IResult<&str, NmeaSentence> {
         sentence.update_checksum()
     };
 
-    Ok((i, sentence))
+    Ok(sentence)
 }
 
 pub fn parse_nmea_sentence(sentence: &str) -> core::result::Result<NmeaSentence, Error<'_>> {
     if sentence.len() > SENTENCE_MAX_LEN {
         Err(Error::SentenceLength(sentence.len()))
     } else {
-        Ok(do_parse_nmea_sentence(sentence)?.1)
+        do_parse_nmea_sentence(sentence)
     }
 }
 
